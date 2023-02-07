d4c2cecc78dfaf6844825a2473af7e2bf05a6578
abhinavnair-1011 <abhivish1011@gmail.com>
2023-02-07 14:58:02 +0530

    for the previous taskes.

---
+++ dom.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let title = document.getElementsByTagName("title")[0];
+    title.innerText = "dom testingg";
+   
+
+    let heading=document.querySelector("h1");
+    heading.style.color="orange";
+    heading.style.fontSize="40px;"
+   heading.backgroundColor="yellow"
+   heading.style.border="10px dotted black"
+   heading.style.textAlign="center"
+   heading.innerHTML="HEADING"
+
+    let inputs= document.querySelectorAll("input");
+       for(let input of inputs){
+       input.style.backgroundColor="blue"
+       input.style.color="cyan";
+    }
+    let listItems= document.querySelectorAll("li");
+   
+    for(let list of listItems){
+        list.innerHTML="CHANGED"
+        list.style.color="brown"
+        list.style.fontSize="25px"
+        list.style.border="5px solid green"
+        list.style.fontWeight="bold";
+       
+    }
+    listItems[2].style.backgroundColor="green";
+
+
+    });
+    

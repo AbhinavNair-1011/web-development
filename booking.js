708e76de6d4c84a163403e17d95a6a25086c13ab
AbhinavNair-1011 <abhivish1011@gmail.com>
2023-03-04 20:27:25 +0530

    storing and getting data from crud, visible on the home page

--- booking.js
+++ booking.js
@@ -5,15 +5,41 @@ let name1=document.querySelector("#name");
 let email1=document.querySelector("#email");
 let phno1=document.querySelector("#phone");
 let date1=document.querySelector("#date");
-let tableBody= document.querySelector("tbody");
-
-let th=document.querySelectorAll("th");
+let tBody= document.querySelector("#tableBody");
 
 
 
 
 
-  let arr=[];
+
+function getFromCrud(){
+
+    axios.get("https://crudcrud.com/api/9c5910c3743b43039d6a2c5fa2fbe4f0/bookingApp")
+    .then((res)=>{
+        
+res.data.forEach((each)=>{
+    let tr=document.createElement("tr");
+ 
+
+    for(prop in each ){
+        if(prop==="delete"){
+           continue;
+        }
+      let td=document.createElement("td");
+      td.appendChild(document.createTextNode(each[prop]))
+      tr.appendChild(td);
+    }
+tBody.appendChild(tr)
+
+    
+
+})
+       
+    })
+
+}
+ 
+  
 
 
     btn.addEventListener("click", (e)=>{
@@ -32,8 +58,10 @@ let th=document.querySelectorAll("th");
       
      
      
-     axios.post("https://crudcrud.com/api/7ac37837ab0f4135aa9f9d13368dd602/bookingApp",userDetails).then((res)=>{
-        console.log(res)
+     axios.post("https://crudcrud.com/api/9c5910c3743b43039d6a2c5fa2fbe4f0/bookingApp",userDetails).then((res)=>{
+       res.forEach((each)=>{
+        
+       })
     })
 
 
@@ -49,10 +77,12 @@ let th=document.querySelectorAll("th");
       email1.value=""
        phno1.value=""
        date1.value=""
+
+
     });
 
         
-
+getFromCrud();
 
 });
     
